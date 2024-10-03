18aff7a2fdb9b2ca664595dcafe25ac994417140
Ezra Donner <pianist4486@yahoo.com>
2024-10-03 08:33:49 -0400

    Tuesday morning

--- froyo.js
+++ froyo.js
@@ -3,13 +3,21 @@ let userInput = prompt(
   "Enter a list of froyo flavors separated by commas:",
   "vanilla,vanilla,vanilla,coffee,coffee,strawberry"
 );
+
+// Split the input string into an array of flavors
 let flavorArray = userInput.split(",");
-let flavorCount = {};
-flavorArray.forEach((flavor) => {
-  if (flavorCount[flavor]) {
-    flavorCount[flavor]++;
+
+// Create an object to hold the flavor counts
+let flavorCount = flavorArray.reduce((countObj, flavor) => {
+  // If the flavor is already a key in the object, increment its count
+  if (countObj[flavor]) {
+    countObj[flavor]++;
   } else {
-    flavorCount[flavor] = 1;
+    // If it's not already a key, set its count to 1
+    countObj[flavor] = 1;
   }
-});
+  return countObj;
+}, {});
+
+// Log the resulting object to the console
 console.log(flavorCount);
