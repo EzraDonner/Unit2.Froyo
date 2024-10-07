f6b372dd8f6b729d23fff453748819f9f9c563a9
Ezra Donner <pianist4486@yahoo.com>
2024-10-07 10:52:29 -0400

    Monday morning

--- froyo.js
+++ froyo.js
@@ -1,23 +1,18 @@
 // Prompt the user for a list of integers separated by commas.
-let userInput = prompt(
+const userInput = prompt(
   "Enter a list of froyo flavors separated by commas:",
   "vanilla,vanilla,vanilla,coffee,coffee,strawberry"
 );
 
-// Split the input string into an array of flavors
-let flavorArray = userInput.split(",");
+const flavorArray = userInput.split(",");
 
-// Create an object to hold the flavor counts
-let flavorCount = flavorArray.reduce((countObj, flavor) => {
-  // If the flavor is already a key in the object, increment its count
+const flavorCount = flavorArray.reduce((countObj, flavor) => {
   if (countObj[flavor]) {
     countObj[flavor]++;
   } else {
-    // If it's not already a key, set its count to 1
     countObj[flavor] = 1;
   }
   return countObj;
 }, {});
 
-// Log the resulting object to the console
 console.log(flavorCount);
